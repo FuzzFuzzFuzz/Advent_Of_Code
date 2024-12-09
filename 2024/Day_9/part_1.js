581c242dbef5fdcf9317a20aa480c44041db4cc8
Brandon S <115183756+FuzzFuzzFuzz@users.noreply.github.com>
2024-12-09 07:07:01 +0000

    Initial Disk Mapping

--- 2024/Day_9/part_1.js
+++ 2024/Day_9/part_1.js
@@ -1,1 +1,39 @@
-main();
+const fs = require("fs");
+
+function parseDiskMap(diskMap) {
+    const blocks = [];
+    let fileID = 0;
+
+    for (let i = 0; i < diskMap.length; i += 2) {
+
+        const fileSize = Number(diskMap[i]);
+        const freeSpace = Number(diskMap[i + 1]);
+
+        // Add file blocks with the file ID
+        for (let j = 0; j < fileSize; j++) {
+            blocks.push(fileID);
+        }
+
+        // Add free space blocks
+        for (let j = 0; j < freeSpace; j++) {
+            blocks.push('.');
+        }
+
+        // Increment file ID
+        fileID++;
+    }
+
+    return blocks;
+}
+
+// Input filepath
+const filePath = '2024/Day_9/input.txt';
+
+// Read input from a text file
+const diskMap = fs.readFileSync(filePath, 'utf8').trim();
+
+// Parse DiskMap from file
+const blocks = parseDiskMap(diskMap);
+console.log("Blocks: " + blocks);
+
+
